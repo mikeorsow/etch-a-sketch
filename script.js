56e33fa647edb40d45cbd75874b9823a636f8638
Mike Orsow <mike.orsow@gmail.com>
2022-07-28 18:48:56 -0700

    Add a button that triggers a prompt to resize grid

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-let gridSize = parseInt(prompt("Enter a grid width", 16));
-const squareSize = Math.round(960/gridSize);
-const container = document.querySelector("div.container");  
+const container = document.querySelector("div.container");
 
-for (let i = 0; i < gridSize * gridSize; i++) {
-  console.log(`square ${i} created!`)
-  const square = document.createElement("div");
-  square.classList.add('square')
-  square.style.width = `${squareSize}px`
-  square.style.height = `${squareSize}px`
-  square.addEventListener("mouseover", (e) => {
-    console.log("MOUSEY MOUSEY");
-    e.target.classList.add("filled");
-  });
+const buildGrid = (gridSize) => {
+  const squareSize = Math.round(960 / gridSize);
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    square.addEventListener("mouseover", (e) => {
+      console.log("MOUSEY MOUSEY");
+      e.target.classList.add("filled");
+    });
 
-  container.appendChild(square);
-}
+    container.appendChild(square);
+  }
+};
+
+const setGridSize = () => {
+  console.log("you did it");
+  const promptGridSize = parseInt(prompt("How mamy squares per side? Max 100", 16)) 
+  if (promptGridSize > 100) return alert(`Sorry, that's tooooo big!`)
+  container.innerHTML = "";
+  buildGrid(promptGridSize);
+};
+
+// default grid size 16x16
+buildGrid(16);
